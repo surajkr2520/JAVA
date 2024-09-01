cbb6d99fe6662c4a3eb1d7b5ebce583ac6ba9d08
Suraj Kumar Sahoo <krsahoosuraj@gmail.com>
2024-09-01 10:21:14 +0530

    Add files via upload

---
+++ rotateKarray.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class rotateKarray {
+
+	public static void main(String[] args) {
+		int arr[]= {2,1,3,5,4,5,8,45,9,3,5};
+		int k=6;
+		for(int i=k;i<arr.length;i++) {
+			System.out.print(" "+arr[i]);
+		}
+		for(int i=0;i<k;i++) {
+			System.out.print(" "+arr[i]);
+		}
+
+	}
+
+}
