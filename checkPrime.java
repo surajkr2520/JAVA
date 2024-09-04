e1383afb276df1ad9ab7793c1f2b98c1107903c5
Suraj Kumar Sahoo <krsahoosuraj@gmail.com>
2024-09-05 00:25:30 +0530

    Add files via upload

---
+++ checkPrime.java
@@ -0,0 +1,18 @@
+package numberSystem;
+
+public class checkPrime {
+	public static boolean isPrime(int num) {
+		//Your code goes here
+		int count=0;
+		for(int i=1;i<=num;i++){
+			if(num % i==0)
+			count++;
+		}
+		if(count>2)
+		return false;
+		else
+		return true;
+	}
+	
+
+}
