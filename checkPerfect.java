d4723a6aecfa9bb29c351de963f34cbfa32e1f5b
Suraj Kumar Sahoo <krsahoosuraj@gmail.com>
2024-09-05 00:48:45 +0530

    Add files via upload

---
+++ checkPerfect.java
@@ -0,0 +1,23 @@
+package numberSystem;
+
+public class checkPerfect {
+
+	public static void main(String[] args) {
+		int n=6;
+		int sum=0;
+		for (int i=1;i<n;i++) {
+			if(n%i==0) {
+				sum+=i;
+			}
+		}
+		if(sum==n) {
+			System.out.println(n+" is a perfect number");
+		}
+		else
+		{
+			System.out.println(n+" is not a perfect number");
+		}
+
+	}
+
+}
