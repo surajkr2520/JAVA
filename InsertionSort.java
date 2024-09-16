895f8cd3d0458a0d2e14b266612a0206dd1926be
Suraj Kumar Sahoo <krsahoosuraj@gmail.com>
2024-09-16 11:46:16 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,22 @@
+package sorting;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		int []arr= {3,5,6,1,2,4};
+		
+			for(int i=1;i<arr.length;i++) {
+				int current=arr[i];
+				int j=i-1;
+				while(j>=0 && current<arr[j]) {
+					arr[j+1]=arr[j];
+					j--;
+				}
+				arr[j+1]=current;
+			}
+			for (int i=0;i<arr.length;i++) {
+System.out.print(" "+arr[i]);}
+	}
+	
+
+}
