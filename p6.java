bb9a9226261e6660ac67dc088c83de95b2ccf0f2
Suraj Kumar Sahoo <krsahoosuraj@gmail.com>
2024-10-11 23:20:39 +0530

    Add files via upload

---
+++ p6.java
@@ -0,0 +1,12 @@
+package Problem6;
+import java.util.*;
+public class p6 {
+
+	public static void main(String[] args) {
+		int arr[]= {2,2,58,60,60};
+		HashMap<Integer,Integer>mp=new HashMap<>();
+		for(int i=-)
+
+	}
+
+}
