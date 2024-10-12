08e81ef03d7cab973c5ac3793c33baa9bf4b2bdd
Suraj Kumar Sahoo <krsahoosuraj@gmail.com>
2024-10-12 21:45:39 +0530

    Update p6.java

--- p6.java
+++ p6.java
@@ -5,7 +5,11 @@ public class p6 {
 	public static void main(String[] args) {
 		int arr[]= {2,2,58,60,60};
 		HashMap<Integer,Integer>mp=new HashMap<>();
-		for(int i=-)
+		for(int i=0;i<arr.length;i++)
+			{
+				
+			}
+				
 
 	}
 
