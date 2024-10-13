2c2b55bc93670891cbcf958aff1f2ec4a165f51d
Suraj Kumar Sahoo <krsahoosuraj@gmail.com>
2024-10-13 20:10:16 +0530

    Add files via upload

--- p6.java
+++ p6.java
@@ -3,13 +3,13 @@ import java.util.*;
 public class p6 {
 
 	public static void main(String[] args) {
-		int arr[]= {2,2,58,60,60};
-		HashMap<Integer,Integer>mp=new HashMap<>();
-		for(int i=0;i<arr.length;i++)
-			{
-				
-			}
-				
+		int arr[]= {10,20,30,40,50,60};
+		int sum = 0;
+		for(int i=arr.length-1;i>=0;i-=2)
+		{
+			sum += arr[i];
+		}
+		System.out.println(sum);
 
 	}
 
